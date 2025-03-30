86caea8f2b9ec15d6440d156a2afa46c89158254
tib77390 <thibdupas@gmail.com>
2025-03-30 16:47:45 +0200

    Ajout systéme routage

---
+++ Router/Route.js
@@ -0,0 +1,15 @@
+export default class Route {
+    
+        constructor(url, title, pathHtml, pathJS = "") {
+    
+          this.url = url;
+    
+          this.title = title;
+    
+          this.pathHtml = pathHtml;
+    
+          this.pathJS = pathJS;
+    
+        }
+    
+    }
---
+++ Router/allRoutes.js
@@ -0,0 +1,14 @@
+
+import Route from "quai antique front/Route.js";
+
+
+//Définir ici vos routes
+
+export const allRoutes = [
+
+    new Route("/", "Accueil", "/pages/home.html"),];
+
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+
+export const websiteName = "Quai Antique";
